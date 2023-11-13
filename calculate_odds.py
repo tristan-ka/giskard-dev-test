2cdeed7d56a9448369103a3f780eaf07af69e22e
tristan-ka <tristan.karch@gmail.com>
2023-11-13 18:16:34 +0100

    updated dikstra to avoid revisiting visited states and added functions to count encounters and compute odds to reach destination

--- calculate_odds.py
+++ calculate_odds.py
@@ -1,4 +1,6 @@
 import heapq
+import numpy as np
+
 
 def convert_to_undirected_graph(graph):
     undirected_graph = {}
@@ -13,42 +15,44 @@ def convert_to_undirected_graph(graph):
                 undirected_graph[neighbor] = []
             undirected_graph[neighbor].append((node, weight))
 
+    # add self-directed edges
     for node in undirected_graph:
         undirected_graph[node].append((node, 1))
     return undirected_graph
 
 
-def compute_min_days(graph, start, destination, car_autonomy, count_down):
-    '''
+def convert_bounty_hunters_dict(bounty_hunters):
+    out_dict = {}
+    for entry in bounty_hunters:
+        out_dict.setdefault(entry["planet"], []).append(entry["day"])
+    return out_dict
 
-    :param graph:
-    :type graph:
-    :param start:
-    :type start:
-    :param destination:
-    :type destination:
-    :param car_autonomy:
-    :type car_autonomy:
-    :param count_down:
-    :type count_down:
-    :return:
-    :rtype:
-    '''
+
+def compute_paths(graph, start, destination, car_autonomy, count_down):
+    """
+    Transversal search of the graph of the universe to output all the possible path starting form planet start
+    and reaching planet destination considering car_autonomy and before count_down
+    """
 
     # Create a priority queue to keep track of stuff
     pq = [(0, start, car_autonomy, [start], [0], [])]  # (time, planet, car_autonomy, path, time_path, refuel_mask)
-
-    possible_path = []
+    visited_states = set()
+    possible_paths = []
     while pq:
 
         time, planet, current_autonomy, path, time_path, refuel_mask = heapq.heappop(pq)
 
+        # Check if the current state has been visited
+        state = (planet, current_autonomy, tuple(path))
+        if state in visited_states:
+            continue
+        visited_states.add(state)
+
         if planet == destination and time <= count_down:
-            heapq.heappop(pq)
-            possible_path.append((time, path, time_path, refuel_mask))
+            possible_paths.append({'time': time, 'path': path, 'time_path': time_path, 'refuel_mask': refuel_mask})
 
         if time > count_down:
-            return possible_path
+            return possible_paths
 
         for neighbor, time_to_neighbor in graph[planet]:
             if time_to_neighbor <= current_autonomy:
@@ -71,7 +75,26 @@ def compute_min_days(graph, start, destination, car_autonomy, count_down):
             new_refuel_mask = refuel_mask + [refuel]
             heapq.heappush(pq, (new_time, new_planet, new_autonomy, new_path, new_time_path, new_refuel_mask))
 
-    return possible_path  # Destination is not reachable
+    return possible_paths  # Destination is not reachable
+
+
+def count_encounters(possible_paths, hunters_dict):
+    """
+    given the list of possible paths and bounty_hunters info count the number of encounters with hunters and calculate
+    the associated proba to reach destination.
+    """
+    for path in possible_paths:
+        counter = 0
+        for planet, day in zip(path['path'], path['time_path']):
+            if planet in hunters_dict.keys():
+                if day in hunters_dict[planet]:
+                    counter += 1
+        path['counter'] = counter
+        if counter >= 1:
+            path['proba'] = 1 - np.sum([9 ** i / (10 ** (i + 1)) for i in range(counter)])
+        else:
+            path['proba'] = 1
+    return possible_paths
 
 
 if __name__ == '__main__':
@@ -90,12 +113,22 @@ if __name__ == '__main__':
         {"planet": "Hoth", "day": 7},
         {"planet": "Hoth", "day": 8}
     ]
+    hunters_dict = convert_bounty_hunters_dict(bounty_hunters)
+
     start_planet = 'Tatooine'
     destination_planet = 'Endor'
     car_autonomy = 6
 
-    count_down = 8
-    possible_paths = compute_min_days(graph, start_planet, destination_planet, car_autonomy, count_down)
+    count_down = 10
+    possible_paths = compute_paths(graph, start_planet, destination_planet, car_autonomy, count_down)
+    possible_paths = count_encounters(possible_paths, hunters_dict)
+    if possible_paths:
+        index_path = np.argmax([possible_paths[i]['proba'] for i in range(len(possible_paths))])
+        proba = possible_paths[index_path]['proba']
+        path = possible_paths[index_path]['path']
+    else:
+        proba = 0
+        path = []
 
-    print(possible_paths)
-    print(len(possible_paths))
+    print(proba)
+    print(path)
